6b4464b03c19d18fef77146ede9d41998b3c8686
Sam Stavinoha <smlstvnh@gmail.com>
2015-05-20 18:02:49 -0500

    update runme

--- runme.sh
+++ runme.sh
@@ -6,54 +6,64 @@ VUNDLE_VERSION='v0.10.2'
 mkdir -v -p ~/.autocomplete
 
 # fetch autocomplete scripts
-echo "Fetching git autocomplete script."
+echo -e "\n *** Fetching git autocomplete script. ***"
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash \
   -o ~/.autocomplete/git-completion.bash
 
-echo "Fetching mercurial autocomplete script."
+echo -e "\n *** Fetching mercurial autocomplete script. ***"
 curl http://www.selenic.com/hg/raw-file/tip/contrib/bash_completion \
   -o ~/.autocomplete/hg-completion.bash
 
-echo "Fetching tmux autocomplete script."
+echo -e "\n *** Fetching tmux autocomplete script. ***"
 curl https://raw.githubusercontent.com/ThomasAdam/tmux/master/examples/bash_completion_tmux.sh \
   -o ~/.autocomplete/tmux-completion.bash
 
-
+#
 # links
+#
 
 # tmux
-echo "Creating link ~/.tmux.conf"
+echo -e "\n *** Creating link ~/.tmux.conf ***"
 ln -v -nf -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # git
-echo "Creating link ~/.gitconfig"
-ln -v -nf -s ~/dotfiles/git/.gitconfig ~/.gitconfig
+echo -e "\n *** Creating link ~/.gitconfig ***"
+ln -v -nf -s ~/dotfiles/git/gitconfig ~/.gitconfig
 
 # python
-echo "Creating link ~/.pystartup"
-ln -v -nf -s ~/dotfiles/.pystartup ~/.pystartup
+echo -e "\n *** Creating link ~/.pystartup ***"
+ln -v -nf -s ~/dotfiles/python/pystartup ~/.pystartup
+echo -e "\n *** Creating link ~/.pystartup/.pythonrc ***"
+ln -v -nf -s ~/dotfiles/python/pystartup/pythonrc ~/.pystartup/.pythonrc
+echo -e "\n *** Creating link ~/.pdbrc.py ***"
+ln -v -nf -s ~/dotfiles/python/pdbrc.py ~/.pdbrc.py
 
 # profile
-#for f in ~/dotfiles/bashrc/.bash*; do
-echo "Linking autocomplete scripts."
-ln -v -nf -s ~/dotfiles/bashrc/.bash* ~
+#for f in ~/dotfiles/bash/.bash*; do
+echo -e "\n *** Linking autocomplete scripts. ***"
+ln -v -nf -s ~/dotfiles/bash/bash_profile ~/.bash_profile
+ln -v -nf -s ~/dotfiles/bash/bashrc ~/.bashrc
+ln -v -nf -s ~/dotfiles/bash/bash_aliases ~/.bash_aliases
 
 # vim & vundle
 VUNDLEPATH=~/.vim/bundle
-echo "Creating link ~/.vim"
+echo -e "\n *** Creating link ~/.vim ***"
 ln -v -nf -s ~/dotfiles/.vim ~/.vim
-echo "Cloning gmarik/Vundle.vim"
-git clone https://github.com/gmarik/Vundle.vim.git ${VUNDLEPATH}/Vundle.vim.temp
+echo -e "\n *** Cloning git repo: gmarik/Vundle.vim ***"
+git clone --progress --verbose https://github.com/gmarik/Vundle.vim.git \
+    ${VUNDLEPATH}/Vundle.vim.temp
 rm -rf ${VUNDLEPATH}/Vundle.vim
 mv -v -f ${VUNDLEPATH}/Vundle.vim.temp ${VUNDLEPATH}/Vundle.vim
-echo "Fetching Vundle versions..."
-git --work-tree ${VUNDLEPATH}/Vundle.vim --git-dir ${VUNDLEPATH}/Vundle.vim/.git fetch --all
-echo "Checking out Vundle $VUNDLE_VERSION"
+echo -e "\n *** Fetching Vundle versions... ***"
+git --work-tree ${VUNDLEPATH}/Vundle.vim --git-dir ${VUNDLEPATH}/Vundle.vim/.git fetch --force --update-head-ok --verbose --all --tags
+echo -e "\n *** Checking out Vundle $VUNDLE_VERSION ***"
 git --work-tree ${VUNDLEPATH}/Vundle.vim --git-dir ${VUNDLEPATH}/Vundle.vim/.git reset --hard $VUNDLE_VERSION
 
+
+echo -e "\n *** Creating link ~/.vimrc ***"
 ln -v -nf -s ~/dotfiles/.vimrc ~/.vimrc
 
 # install Vundle plugins
+echo -e "\n *** Installing Vundle plugins and restarting session... ***"
 vim +PluginInstall +qall
-
-exec bash -l
+exec bash --login
