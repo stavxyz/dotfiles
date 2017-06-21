8e9c3b9caf72ce8a5ce702e8b321b3a1a3b225b9
Sam Stavinoha <smlstvnh@gmail.com>
2017-06-21 12:52:43 -0400

    osx things

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -16,7 +16,7 @@ alias g='git'
 
 
 # misc. aliases
-alias rgrep='grep --exclude-dir .terraform --exclude-dir node_modules --exclude-dir dist --exclude-dir .git --exclude-dir .tox -I -r -n -i -e'
+alias rgrep='grep --exclude .babel.json --exclude-dir .terraform --exclude-dir node_modules --exclude-dir dist --exclude-dir .git --exclude-dir .tox -I -r -n -i -e'
 findfile () {
   echo -e "Looking for regular file $1, ignoring hidden directories.\n"
   find . -not -path '*/\.*' -type f -iname $1
