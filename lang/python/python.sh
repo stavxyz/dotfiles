19b0bbedd969bf9dcc2732b2454cb60b07b64630
Sam Stavinoha <smlstvnh@gmail.com>
2018-02-02 11:14:22 -0600

    use pyenv virtualenvs

--- lang/python/python.sh
+++ lang/python/python.sh
@@ -11,6 +11,8 @@ alias plint="pylint --msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {
 alias pipi="pip install --upgrade --force-reinstall --no-cache-dir $1"
 
 # pyenv
+export PYENV_VIRTUALENVWRAPPER_PREFER_PYVENV="true"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
+pyenv virtualenvwrapper
