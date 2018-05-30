7bc2d61b95eaa3d3c33369baf63d60904456768e
Sam Stavinoha <smlstvnh@gmail.com>
2018-05-30 13:36:09 -0400

    shopt -s checkwinsize

--- bash/history.sh
+++ bash/history.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
-export HISTCONTROL=ignoredups:erasedups
-export HISTSIZE=100000
-export HISTFILESIZE=100000
+export HISTCONTROL=ignoredups:ignorespace
 shopt -s histappend
-
-# Save and reload the history with the `h` command
-function h {
-    history -a
-    history -c
-    history -r
-}
+HISTSIZE=10000
+HISTFILESIZE=20000
+shopt -s checkwinsize
