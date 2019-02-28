671e24a90edd1364a1bcc1afc154e55ef4093e03
Sam Stavinoha <smlstvnh@gmail.com>
2019-02-28 14:14:58 -0600

    prepend new homebrew libexec/bin dir on PATH

--- homebrew/homebrew.sh
+++ homebrew/homebrew.sh
@@ -5,3 +5,13 @@ export HOMEBREW_GITHUB_API_TOKEN=`keyring get homebrew github_api_token`
 if [ -f $(brew --prefix)/etc/bash_completion ]; then
   source $(brew --prefix)/etc/bash_completion
 fi
+
+#### From `brew info python` ###
+#
+# Unversioned symlinks `python`, `python-config`, `pip` etc. pointing to
+# `python3`, `python3-config`, `pip3` etc., respectively, have been installed into
+#  /usr/local/opt/python/libexec/bin
+#
+################################
+
+export PATH=/usr/local/opt/python/libexec/bin:$PATH
