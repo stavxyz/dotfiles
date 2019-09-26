d7b9016a6b702496285a991249c3b2d488c093c4
Sam Stavinoha <smlstvnh@gmail.com>
2019-09-26 11:24:40 -0500

    ssh agent

---
+++ bash/ssh.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+eval "$(ssh-agent -s)"
+for _key in $(ls -A ~/.ssh/*.pub); do
+    # %???? removes '.pub' to target
+    # corresponding private key
+    _priv="${_key%????}"
+    ssh-add "${_priv}"
+done
