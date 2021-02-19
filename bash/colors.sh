52fab895dddcb63517cca7cb36cb04740f8f9517
Sam Stavinoha <smlstvnh@gmail.com>
2021-02-19 00:35:58 +0000

    cleanup from ubuntu experiment

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-force_color_prompt=yes
-
 ################ BASE16_SHELL #######################
 # A shell script to change your shell's default ANSI
 # colors but most importantly, colors 17 to 21 of
@@ -48,67 +46,3 @@ function go-dark {
 function let-there-be-light {
   theme-switch light
 }
-
-
-# Tell ls to be colorful
-export CLICOLOR=1
-export LSCOLORS=exFxCxDxBxegedabagaced
-
-if [[ $OSTYPE == *"linux"* ]]; then
-  alias ls='ls --color=auto'
-elif [[ $OSTYPE == *"darwin"* ]]; then
-  alias ls='ls -aGFh'
-  alias la='ls -alsG'
-fi
-
-
-#export LSCOLORS=ExFxBxDxCxegedabagacad
-
-# Tell grep to highlight matches
-export GREP_OPTIONS='--color=auto'
-
-#name terminal tabs
-#export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}\007"'
-export PROMPT_COMMAND='echo -ne "\033]0;${PWD##*/}-(${VIRTUAL_ENV##*/})\007"'
-
-########################
-## COLOR/CUSTOMIZE PROMPT
-########################
-export RED="\[\e[31m\]"
-export GREEN="\[\e[32m\]"
-export YELLOW="\[\e[33m\]"
-export BLUE="\[\e[34m\]"
-export PURPLE="\[\e[35m\]"
-export LTBLUE="\[\e[36m\]"
-export WHITE="\[\e[37m\]"
-export RESET="\[\e[0m\]"
-
-if [ -f "/etc/debian_version" ]; then
-    export IS_DEBIAN=1
-    export ICON="🌀";
-fi
-
-cd () { builtin cd "$@" && chpwd; }
-pushd () { builtin pushd "$@" && chpwd; }
-popd () { builtin popd "$@" && chpwd; }
-chpwd () {
-  case $PWD in
-    $HOME) HPWD="~";;
-    $HOME/*/*) HPWD="${PWD#"${PWD%/*/*}/"}";;
-    $HOME/*) HPWD="~/${PWD##*/}";;
-    /*/*/*) HPWD="${PWD#"${PWD%/*/*}/"}";;
-    *) HPWD="$PWD";;
-  esac
-}
-#PS1='$HPWD \$'
-
-function hpwd {
-    echo $HPWD
-}
-
-export PS1="${PURPLE}[${BLUE}\$(hpwd)${PURPLE}] ${RED}\u${PURPLE}@${YELLOW}\h${PURPLE}${ICON}\n\$ ${RESET}"
-cd  # this is to trigger evaluation of chpwd when shell comes up
-#################
-
-# enable system bash completion
-[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
--- bash/ssh.sh
+++ bash/ssh.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
 eval "$(ssh-agent -s)"
-for _key in $(ls -A ~/.ssh/*.pub); do
-    # %???? removes '.pub' to target
-    # corresponding private key
-    _priv="${_key%????}"
-    ssh-add -q "${_priv}"
-done
+
+if [ ! -d "~/.ssh" ]; then
+  echo "~/.ssh dir DNE, skipping."
+else
+  for _key in $(ls -A ~/.ssh/*.pub); do
+      # %???? removes '.pub' to target
+      # corresponding private key
+      _priv="${_key%????}"
+      ssh-add -q "${_priv}"
+  done
+fi
