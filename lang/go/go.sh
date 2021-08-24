59d6bf5bec436e49b5db1875ba12f1f0f32d4bf8
Sam Stavinoha <smlstvnh@gmail.com>
2021-08-24 05:11:57 +0000

    go

--- lang/go/go.sh
+++ lang/go/go.sh
@@ -2,12 +2,15 @@
 
 export GOPATH=$HOME/go
 # for brew installed go
-export GOROOT=/usr/local/opt/go/libexec
+#export GOROOT=/usr/local/opt/go/libexec
 export PATH=$PATH:$GOPATH/bin
 export PATH=$PATH:$GOROOT/bin
 
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+
 # Toggle module support
-export GO111MODULE='on'
+#export GO111MODULE='on'
 
 # gore
 alias gore='gore -autoimport'
