dce238b6c0c4223d681ef267dfd799603034f998
Sam Stavinoha <smlstvnh@gmail.com>
2021-08-25 23:51:09 -0500

    use path setup from "n" docs
    this should check for the 'keyring' command
    as well as the existence of the github api token
    within the keyring
    
    if not available, log a message

--- lang/node/node.sh
+++ lang/node/node.sh
@@ -6,5 +6,4 @@ alias nom="rm -rf node_modules && npm cache clear && npm install"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh" # This loads nvm
 
-export N_PREFIX="$HOME/n"
-export PATH="${PATH}:${N_PREFIX}/bin"
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
