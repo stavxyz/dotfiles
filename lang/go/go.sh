7b12c769f21c0bfdaa07e047992c586101a4aa61
Sam Stavinoha <smlstvnh@gmail.com>
2021-09-27 12:25:40 -0500

    go updates

--- lang/go/go.sh
+++ lang/go/go.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 export GOPATH=$HOME/go
-# for brew installed go
-#export GOROOT=/usr/local/opt/go/libexec
+export GOBIN=$GOPATH/bin
+mkdir -p "$GOBIN"
+
+if [[ $OSTYPE =~ "darwin" ]]; then
+  :
+  # for brew installed go
+  #export GOROOT="$(brew --prefix go)/libexec"
+  #export PATH=$PATH:$GOROOT/bin
+  # commented because I dont think this is necessary
+fi
+
 export PATH=$PATH:$GOPATH/bin
-export PATH=$PATH:$GOROOT/bin
 
 # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 
 # Toggle module support
-#export GO111MODULE='on'
+export GO111MODULE='auto'
 
 # gore
 alias gore='gore -autoimport'
