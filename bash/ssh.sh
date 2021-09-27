0c5d3e358cf00468fe92106e3cde414f215ad1ad
Sam Stavinoha <smlstvnh@gmail.com>
2021-09-27 12:25:18 -0500

    dont try to add files that dont exist

--- bash/ssh.sh
+++ bash/ssh.sh
@@ -3,12 +3,12 @@
 eval "$(ssh-agent -s)" > /dev/null
 
 if ! [[ -d "${HOME}/.ssh" ]]; then
-  errcho "~/.ssh dir DNE, skipping."
+  errcho "${HOME}/.ssh dir DNE, skipping."
 else
-  for _key in $(ls -A ~/.ssh/*.pub); do
+  for _key in $(ls -1A ~/.ssh/*.pub); do
       # %???? removes '.pub' to target
       # corresponding private key
       _priv="${_key%????}"
-      ssh-add -q "${_priv}"
+      [[ -f "${_priv}" ]] && ssh-add -q "${_priv}"
   done
 fi
